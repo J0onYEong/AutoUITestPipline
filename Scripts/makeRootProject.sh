b87f3645278d7ec36ed2662bcc2847187d936dcb
J0onYEong <cjy2134@naver.com>
2025-12-07 13:41:55 +0900

    [Chore] tuist 경로 출력

--- Scripts/makeRootProject.sh
+++ Scripts/makeRootProject.sh
@@ -1,19 +1,11 @@
 #!/bin/zsh
 
-export PATH="/Users/choejun-yeong/.local/bin:$PATH"
-
-echo "Current Shell: $SHELL"
-
+export PATH="$HOME/.local/share/mise/shims:$HOME/.local/share/mise/bin:$PATH"
 cd Junios
 
 echo "#1. configure mise"
 eval "$(mise activate)"
-
-mise install tuist@4.90.0
-
 mise use tuist@4.90.0
-
-which tuist
 
 echo "#2. Tuist install"
 tuist install
