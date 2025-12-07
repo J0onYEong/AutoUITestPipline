8ffd4b2eb0baf6b37404e04cd27d111fb0181e40
J0onYEong <cjy2134@naver.com>
2025-12-07 13:25:54 +0900

    [Feat] Tuist버전 지정 코드 추가

--- Scripts/makeRootProject.sh
+++ Scripts/makeRootProject.sh
@@ -4,11 +4,12 @@ export PATH="/Users/choejun-yeong/.local/bin:$PATH"
 
 cd Junios
 
-echo "#1. Activate mise"
+echo "#1. configure mise"
 eval "$(mise activate zsh)"
+mise use tuist@4.90.0
 
 echo "#2. Tuist install"
 tuist install
 
 echo "#3. Tuist generate"
-tuist generate
+tuist generate --no-open
