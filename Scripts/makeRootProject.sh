49e4719dcddf4f881acdbe7099f8a1f0fcd2309b
junyeong-personal <cjy2134@naver.com>
2025-12-08 13:53:30 +0900

    [Test] mise 경로 확인

--- Scripts/makeRootProject.sh
+++ Scripts/makeRootProject.sh
@@ -2,13 +2,15 @@
 
 export PATH="$HOME/.local/bin:$HOME/.local/share/mise/shims:$HOME/.local/share/mise/bin:$PATH"
 
+which mise
+
 cd Junios
 
 echo "[Tuist] configuration"
 mise use tuist@4.90.0
 
 echo "[Tuist] Install xcode project dependencies"
-tuist install
+mise exec tuist install
 
 echo "[Tuist] Generate project"
-tuist generate --no-open
+mise exec tuist generate --no-open
