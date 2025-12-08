4f9b80e66519341cbbef3e9d8ebda8aa7c498f20
junyeong-personal <cjy2134@naver.com>
2025-12-08 18:32:38 +0900

    [Chore] 코멘트 생성 스크립트 완성

---
+++ Scripts/createUniquPRComment.sh
@@ -0,0 +1,37 @@
+#!/bin/zsh
+
+UNIQUE_TITLE=$1
+COMMENT_BODY=$2
+GIT_TOKEN=$3
+PR_NUMBER=$4
+
+BASE_URL="https://api.github.com/repos/J0onYEong/AutoUITestPipline"
+
+# 코맨트 획득
+RES_COMMENTS=$(curl -L \
+  -X GET \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GIT_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  ${BASE_URL}/issues/comments)
+
+# 코맨트 삭제
+echo "$RES_COMMENTS" | yq '.[] | select(.body | contains("'"## ${UNIQUE_TITLE}"'")) | .id' | while read -r COMMENT_ID; do
+  curl -L \
+    -X DELETE \
+    -H "Accept: application/vnd.github+json" \
+    -H "Authorization: Bearer ${GIT_TOKEN}" \
+    -H "X-GitHub-Api-Version: 2022-11-28" \
+    ${BASE_URL}/issues/comments/${COMMENT_ID}
+done
+
+# 새로운 코멘트 생성
+NEW_BODY="## ${UNIQUE_TITLE}\\n${COMMENT_BODY}"
+
+curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${GIT_TOKEN}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  ${BASE_URL}/issues/${PR_NUMBER}/comments \
+  -d "{\"body\": \"${NEW_BODY}\"}" > /dev/null 2>&1
